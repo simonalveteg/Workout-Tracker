17b3ca3c7e0abe03639ceb7205c926fe586b401f
= <simon.alveteg@gmail.com>
2022-01-15 19:08:01 +0100

    Add webview of musclewiki to ExerciseDetailScreen

--- app/src/main/java/com/example/android/january2022/ui/exercises/detail/ExerciseDetailScreen.kt
+++ app/src/main/java/com/example/android/january2022/ui/exercises/detail/ExerciseDetailScreen.kt
@@ -1,11 +1,16 @@
 package com.example.android.january2022.ui.exercises.detail
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.android.january2022.ui.exercises.ExerciseViewModel
 
@@ -16,4 +21,17 @@ fun ExerciseDetailScreen(
     Box(modifier = Modifier.padding(48.dp)) {
         Text(text = viewModel.currentExercise.toString())
     }
+    val url = "https://www.musclewiki.com/Stretches/Male/Biceps#Biceps-Stretch"
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
 }
