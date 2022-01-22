7b34cc8d8c0c88a57b04e443b730c4a10588cd52
= <simon.alveteg@gmail.com>
2022-01-22 13:23:13 +0100

    fixed animations of sessioncard on homescreen

--- app/src/main/java/com/example/android/january2022/ui/home/SessionCard.kt
+++ app/src/main/java/com/example/android/january2022/ui/home/SessionCard.kt
@@ -2,6 +2,7 @@ package com.example.android.january2022.ui.home
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.android.january2022.R
@@ -39,7 +41,10 @@ fun SessionCard(
         "HH:mm",
         Locale.ENGLISH
     ).format(session.startTimeMilli)
-    val muscleGroups = viewModel.getMuscleGroupsForSession(session.sessionId).collectAsState(initial = emptyList())
+    val muscleGroups =
+        viewModel.getMuscleGroupsForSession(session.sessionId).collectAsState(initial = emptyList())
+    val iconRotation = animateFloatAsState(targetValue = if(expanded) 180f else 0f)
+
 
     Card(
         modifier = Modifier
@@ -50,14 +55,22 @@ fun SessionCard(
             }
             .animateContentSize(
                 animationSpec = tween(
-                    durationMillis = 300,
-                    delayMillis = 50,
+                    durationMillis = 100,
+                    delayMillis = 5,
                     easing = LinearOutSlowInEasing
                 )
             )
     ) {
         Column(
-            Modifier.padding(vertical = 16.dp, horizontal = 16.dp)
+            Modifier
+                .padding(vertical = 16.dp, horizontal = 16.dp)
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 100,
+                        delayMillis = 5,
+                        easing = LinearOutSlowInEasing
+                    )
+                )
         ) {
             Row {
                 Column {
@@ -76,7 +89,7 @@ fun SessionCard(
                     }
                 ) {
                     Icon(
-                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        imageVector = Icons.Filled.ExpandMore,
                         contentDescription =
                         if (expanded) {
                             stringResource(R.string.show_less)
@@ -84,7 +97,8 @@ fun SessionCard(
                             stringResource(
                                 R.string.show_more
                             )
-                        }
+                        },
+                        modifier = Modifier.rotate(iconRotation.value)
                     )
                 }
             }
@@ -92,11 +106,10 @@ fun SessionCard(
                 Box(Modifier.padding(top = 12.dp, bottom = 8.dp, start = 0.dp, end = 0.dp)) {
                     Divider(color = MaterialTheme.colors.onSurface, thickness = 1.dp)
                 }
-                sessionContent.forEach { sessionExercise ->
-                    if (sessionExercise.sessionExercise.parentSessionId == session.sessionId) {
+                sessionContent.filter { it.sessionExercise.parentSessionId == session.sessionId }
+                    .forEach { sessionExercise ->
                         SessionContent(sessionExercise, sets)
                     }
-                }
             }
         }
     }
