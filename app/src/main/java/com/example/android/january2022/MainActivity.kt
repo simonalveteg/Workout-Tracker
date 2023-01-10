73bf43756771a64f5d254c5eb4615752f4970752
= <simon.alveteg@gmail.com>
2023-01-10 22:47:19 +0100

    Removed dependencies, added scaffold to sessionscreen

--- app/src/main/java/com/example/android/january2022/MainActivity.kt
+++ app/src/main/java/com/example/android/january2022/MainActivity.kt
@@ -3,25 +3,20 @@ package com.example.android.january2022
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.core.view.WindowCompat
-import androidx.navigation.*
+import androidx.navigation.compose.rememberNavController
 import com.example.android.january2022.ui.rework.NavHost
 import com.example.compose.WorkoutTheme
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-  @OptIn(ExperimentalAnimationApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       WorkoutTheme {
-        val navController = rememberAnimatedNavController()
+        val navController = rememberNavController()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         NavHost(navController)
       }
--- app/src/main/java/com/example/android/january2022/ui/rework/SessionScreen.kt
+++ app/src/main/java/com/example/android/january2022/ui/rework/SessionScreen.kt
@@ -1,11 +1,41 @@
 package com.example.android.january2022.ui.rework
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.android.january2022.utils.UiEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SessionScreen(
   onNavigate: (UiEvent.Navigate) -> Unit,
 ) {
+  Scaffold(
+    bottomBar = {
+      BottomAppBar() {
+
+      }
+    }
+  ) { paddingValues ->
+    LazyColumn(
+      modifier = Modifier.padding(paddingValues = paddingValues)
+    ) {
+      items(4) {
+        Surface(
+          modifier = Modifier.fillMaxWidth().height(100.dp).padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+
+        }
+      }
+    }
+  }
 
 }
