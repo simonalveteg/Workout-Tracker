7004824b61a4da19e49ef704a7ad7303cd1e9f4a
= <simon.alveteg@gmail.com>
2023-01-10 23:33:58 +0100

    Add FAB to bottombar. Colors are wrong??

--- app/src/main/java/com/example/android/january2022/ui/rework/SessionScreen.kt
+++ app/src/main/java/com/example/android/january2022/ui/rework/SessionScreen.kt
@@ -1,13 +1,13 @@
 package com.example.android.january2022.ui.rework
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,21 +18,34 @@ import com.example.android.january2022.utils.UiEvent
 fun SessionScreen(
   onNavigate: (UiEvent.Navigate) -> Unit,
 ) {
+
   Scaffold(
     bottomBar = {
-      BottomAppBar() {
-
-      }
+      BottomAppBar(
+        actions = {},
+        floatingActionButton = {
+          FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Exercise")
+          }
+        }
+      )
     }
   ) { paddingValues ->
     LazyColumn(
-      modifier = Modifier.padding(paddingValues = paddingValues)
+      modifier = Modifier
+        .padding(paddingValues = paddingValues)
+        .fillMaxSize()
     ) {
-      items(4) {
+      items(10) {
         Surface(
-          modifier = Modifier.fillMaxWidth().height(100.dp).padding(vertical = 4.dp, horizontal = 8.dp)
+          modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(vertical = 4.dp, horizontal = 8.dp),
+          tonalElevation = 1.dp,
+          shape = MaterialTheme.shapes.medium
         ) {
-
+          Text(text = "This is an exercise")
         }
       }
     }
