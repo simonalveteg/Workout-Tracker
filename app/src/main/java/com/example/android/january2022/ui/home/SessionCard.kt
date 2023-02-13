c96f1e1c15549607030198edee858e95b461504a
= <simon.alveteg@gmail.com>
2023-02-13 22:03:27 +0100

    Improced musclegroup text generation

--- app/src/main/java/com/example/android/january2022/ui/home/SessionCard.kt
+++ app/src/main/java/com/example/android/january2022/ui/home/SessionCard.kt
@@ -2,9 +2,7 @@ package com.example.android.january2022.ui.home
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,36 +17,44 @@ fun SessionCard(
 
   val session = sessionWrapper.session
   val muscleGroups by sessionWrapper.muscleGroups.collectAsState(initial = emptyList())
+  val muscleTitle by remember {
+    derivedStateOf {
+      if (muscleGroups.isNotEmpty()) muscleGroups[0].uppercase() else ""
+    }
+  }
+  val muscleSubtitle by remember {
+    derivedStateOf {
+      muscleGroups.drop(1).take(3).toString().drop(1).dropLast(1).uppercase()
+    }
+  }
 
   Surface(
     onClick = { onClick() },
     modifier = Modifier
-      .fillMaxWidth().padding(bottom = 8.dp).requiredHeight(75.dp),
+      .fillMaxWidth()
+      .padding(bottom = 8.dp)
+      .requiredHeight(75.dp),
     shape = MaterialTheme.shapes.medium
   ) {
     Row(
-      modifier = Modifier.fillMaxWidth().padding(12.dp),
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(12.dp),
       verticalAlignment = Alignment.CenterVertically
     ) {
       SessionDate(session, Modifier.padding(end = 16.dp))
       Column(verticalArrangement = Arrangement.Center) {
         Text(
-          text = if (muscleGroups.isNotEmpty()) muscleGroups[0].uppercase() else "",
+          text = muscleTitle,
           style = MaterialTheme.typography.headlineSmall
         )
         Row {
-          muscleGroups.forEachIndexed { index, string ->
-            if (index in 1..3) {
-              var newString = string
-              if (index in 1..2 && muscleGroups.size >= index + 2) {
-                newString = "$string, "
-              }
-              Text(
-                text = newString.uppercase(),
-                style = MaterialTheme.typography.bodySmall,
-                color = LocalContentColor.current.copy(alpha = 0.7f)
-              )
-            }
+          if (muscleSubtitle.isNotEmpty()) {
+            Text(
+              text = muscleSubtitle,
+              style = MaterialTheme.typography.bodySmall,
+              color = LocalContentColor.current.copy(alpha = 0.7f)
+            )
           }
         }
       }
