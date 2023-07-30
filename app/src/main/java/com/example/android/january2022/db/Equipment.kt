61a532f41d1c497fcefeb7e3efe902ad992d993b
= <simon.alveteg@gmail.com>
2023-07-30 12:47:54 +0200

    Update Equipment to match database

--- app/src/main/java/com/example/android/january2022/db/Equipment.kt
+++ app/src/main/java/com/example/android/january2022/db/Equipment.kt
@@ -2,26 +2,45 @@ package com.example.android.january2022.db
 
 
 object Equipment {
+    const val ATLAS_STONE = "Atlas Stone"
     const val BARBELL = "Barbell"
-    const val MACHINE = "Machine"
-    const val DUMBBELLS = "Dumbbell"
     const val BODYWEIGHT = "Bodyweight"
+    const val BROOM_STICK = "Broom Stick"
     const val CABLE = "Cable"
+    const val DUMBBELLS = "Dumbbell"
+    const val KETTLEBELL = "Kettlebell"
     const val LEVER = "Lever"
-    const val WEIGHTED = "Weighted"
-    const val STRETCH = "Stretch"
-    const val NULL = "not specified"
+    const val MACHINE = "Machine"
+    const val MEDICINE_BALL = "Medicine Ball"
+    const val RESISTANCE_BAND = "Resistance Band"
+    const val ROPE = "Rope"
+    const val ROPE_MACHINE = "Rope Machine"
+    const val SLED = "Sled"
+    const val SMITH_MACHINE = "Smith Machine"
+    const val STABILITY_BALL = "Stability Ball"
+    const val SUSPENSION = "Suspension"
+    const val WEIGHT = "Weight"
 
     fun getAllEquipment(): List<String> {
         return listOf(
+            ATLAS_STONE,
             BARBELL,
-            MACHINE,
-            DUMBBELLS,
             BODYWEIGHT,
+            BROOM_STICK,
             CABLE,
+            DUMBBELLS,
+            KETTLEBELL,
             LEVER,
-            WEIGHTED,
-            STRETCH
+            MACHINE,
+            MEDICINE_BALL,
+            RESISTANCE_BAND,
+            ROPE,
+            ROPE_MACHINE,
+            SLED,
+            SMITH_MACHINE,
+            STABILITY_BALL,
+            SUSPENSION,
+            WEIGHT
         )
     }
 }
